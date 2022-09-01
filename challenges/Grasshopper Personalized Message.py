0753e1d9dd0624d56cd81536c6e780dc6ba82330
24Ryou <a.yousefzadeh24@gmail.com>
2022-09-02 01:33:32 +0430

    update

--- challenges/Grasshopper Personalized Message.py
+++
@@ -1,7 +0,0 @@
-import codewars_test as test
-# -------------------------------- my solution ------------------------------- #
-def greet(name, owner):
-  return 'Hello boss' if name == owner else 'Hello guest'
-# ----------------------------------- test ----------------------------------- #
-test.assert_equals(greet('Daniel', 'Daniel'), 'Hello boss')
-test.assert_equals(greet('Greg', 'Daniel'), 'Hello guest')
