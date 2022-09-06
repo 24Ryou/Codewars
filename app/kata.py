80921eb20b2010f6cd2a7d71b2493b77ec813751
24Ryou <a.yousefzadeh24@gmail.com>
2022-09-06 09:32:34 +0430

    Python  - Moving Zeros To The End

--- app/kata.py
+++ app/kata.py
@@ -1,14 +1,27 @@
-# bit-counting
+# moving-zeros-to-the-end
 import codewars_test as test
 # -------------------------------- MY SOLUTION ------------------------------- #
-def count_bits(n):
-  return sum(1 for x in list(bin(n)[2:]) if x == '1')
+def move_zeros(lst : list):
+  n = len(lst)
+  for x in lst:
+    if x == 0:
+      lst.remove(x)
+      lst.insert(n , x)
+  return lst
 # ------------------------------ CLEVER SOLUTION ----------------------------- #
-def count_bits(n):
-    return bin(n).count("1")
+def move_zeros(arr):
+  l = [i for i in arr if isinstance(i, bool) or i!=0]
+  return l+[0]*(len(arr)-len(l))
+
+def move_zeros(array):
+  return sorted(array, key=lambda x: x==0)
 # ----------------------------------- TEST ----------------------------------- #
-test.assert_equals(count_bits(0), 0)
-test.assert_equals(count_bits(4), 1)
-test.assert_equals(count_bits(7), 3)
-test.assert_equals(count_bits(9), 2)
-test.assert_equals(count_bits(10), 2)
+test.assert_equals(move_zeros(
+    [1, 2, 0, 1, 0, 1, 0, 3, 0, 1]),
+    [1, 2, 1, 1, 3, 1, 0, 0, 0, 0])
+test.assert_equals(move_zeros(
+    [9, 0, 0, 9, 1, 2, 0, 1, 0, 1, 0, 3, 0, 1, 9, 0, 0, 0, 0, 9]),
+    [9, 9, 1, 2, 1, 1, 3, 1, 9, 9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+test.assert_equals(move_zeros([0, 0]), [0, 0])
+test.assert_equals(move_zeros([0]), [0])
+test.assert_equals(move_zeros([]), [])
