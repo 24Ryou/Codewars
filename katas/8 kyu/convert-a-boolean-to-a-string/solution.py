f9b61bc7453e966f73423aa44a08ca37a90e49bc
24Ryou <a.yousefzadeh24@gmail.com>
2022-09-08 11:08:34 +0430

    PHP - Convert a Boolean to a String

--- katas/8 kyu/convert-a-boolean-to-a-string/solution.py
+++ katas/8 kyu/convert-a-boolean-to-a-string/solution.py
@@ -1,13 +1,17 @@
+# human-readable-time
 import codewars_test as test
 # -------------------------------- MY SOLUTION ------------------------------- #
-boolean_to_string = str
+def make_readable(seconds):
+  h = seconds/3600
+  m = (h - int(h)) * 60
+  s = (m - int(m)) * 60
+  return "{}:{}:{}".format(str(int(h)).zfill(2),str(int(m)).zfill(2),str(round(s)).zfill(2))
 # ------------------------------ CLEVER SOLUTION ----------------------------- #
-def boolean_to_string(b):
-    return str(b)
+def make_readable(s):
+  return '{:02}:{:02}:{:02}'.format(s / 3600, s / 60 % 60, s % 60)
 # ----------------------------------- TEST ----------------------------------- #
-@test.describe("Fixed Tests")
-def fixed_tests():
-    @test.it('Basic Test Cases')
-    def basic_test_cases():
-        test.assert_equals(boolean_to_string(True), "True")
-        test.assert_equals(boolean_to_string(False), "False")
+test.assert_equals(make_readable(0), "00:00:00")
+test.assert_equals(make_readable(5), "00:00:05")
+test.assert_equals(make_readable(60), "00:01:00")
+test.assert_equals(make_readable(86399), "23:59:59")
+test.assert_equals(make_readable(359999), "99:59:59")
