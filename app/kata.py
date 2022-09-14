5a62bbe68598fbf34207a1e0e826780065f56485
24Ryou <a.yousefzadeh24@gmail.com>
2022-09-14 12:44:11 +0430

    Python  - Last digit of a huge number

--- app/kata.py
+++ app/kata.py
@@ -1,17 +1,36 @@
-# does-my-number-look-big-in-this
+# last-digit-of-a-huge-number
 import codewars_test as test
 # -------------------------------- MY SOLUTION ------------------------------- #
-def narcissistic( value ):
-  sum = 0
-  pow = len(str(value))
-  for digit in str(value):
-    sum += int(digit) ** pow
-  return True if sum == value else False
+# watch https://www.youtube.com/watch?v=k7rm55Sw-SE
+# read https://www.geeksforgeeks.org/find-last-digit-of-ab-for-large-numbers/
+def last_digit(lst):
+  if lst == []:
+    return 1
+  r = 1
+  for num in lst[::-1]:
+    r = num ** (r if r < 4 else r % 4 + 4)
+  return r % 10
 # ------------------------------ CLEVER SOLUTION ----------------------------- #
-def narcissistic(value):
-  return value == sum(int(x) ** len(str(value)) for x in str(value))
+def last_digit(lst):
+    n = 1
+    for x in reversed(lst):
+        n = x ** (n if n < 4 else n % 4 + 4)
+    return n % 10
 # ----------------------------------- TEST ----------------------------------- #
-test.assert_equals(narcissistic(7), True, '7 is narcissistic');
-test.assert_equals(narcissistic(371), True, '371 is narcissistic');
-test.assert_equals(narcissistic(122), False, '122 is not narcissistic')
-test.assert_equals(narcissistic(4887), False, '4887 is not narcissistic')
+test.it('Basic tests')
+test_data = [
+    ([], 1),
+    ([0, 0], 1),
+    ([0, 0, 0], 0),
+    ([1, 2], 1),
+    ([3, 4, 5], 1),
+    ([4, 3, 6], 4),
+    ([7, 6, 21], 1),
+    ([12, 30, 21], 6),
+    ([2, 2, 2, 0], 4),
+    ([937640, 767456, 981242], 0),
+    ([123232, 694022, 140249], 6),
+    ([499942, 898102, 846073], 6)
+]
+for test_input, test_output in test_data:
+    test.assert_equals(last_digit(test_input), test_output)
