7e1406d7924a720fc5a7604f8439c0617f36d106
24Ryou <a.yousefzadeh24@gmail.com>
2022-10-07 14:38:47 +0330

    Python  - Snail

--- app/kata.js
+++ app/kata.js
@@ -1,20 +1,17 @@
-// function-1-hello-world
+// grasshopper-personalized-message
 /* ------------------------------- MY SOLUTION ------------------------------ */
-greet = () => 'hello world!'
-
+function greet (name, owner) {
+  return name == owner ? 'Hello boss' : 'Hello guest'
+}
 /* ----------------------------- CLEVER SOLUTION ---------------------------- */
 /* ---------------------------------- TEST ---------------------------------- */
-// const chai = require("chai");
-// const Test = chai.assert.strictEqual; // replace Test.assertEqual with Test
-// chai.config.truncateThreshold=0;
+const chai = require("chai");
+const Test = chai.assert.strictEqual; // replace Test.assertEqual with Test
+chai.config.truncateThreshold=0;
 
-const assert = require("chai").assert;
-
-describe("Testing function", function() {
-  it("Is it a function?", function() {
-    assert.strictEqual(typeof greet, "function", "greet should be a function");
-  });
-  it("Correct return-value?", function() {
-    assert.strictEqual(greet(), "hello world!");
+describe("Tests", () => {
+  it("test", () => {
+Test(greet('Daniel', 'Daniel'), 'Hello boss')
+Test(greet('Greg', 'Daniel'), 'Hello guest')
   });
 });
