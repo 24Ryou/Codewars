caed12661af744bc1a697d9d275ec81601eb12ac
24Ryou <a.yousefzadeh24@gmail.com>
2022-10-10 15:41:28 +0330

    Python  - Sum of Intervals + PHP - Simple Encryption #1 - Alternating Split

--- app/kata.py
+++ app/kata.py
@@ -18,6 +18,12 @@ def merge(intervals):
 
   return merged
 # ------------------------------ CLEVER SOLUTION ----------------------------- #
+def sum_of_intervals(intervals):
+  s, top = 0, float("-inf")
+  for a,b in sorted(intervals):
+    if top < a: top = a
+    if top < b: s, top = s+b-top, b
+  return s
 # ----------------------------------- TEST ----------------------------------- #
 @test.describe("Fixed tests")
 def fixed_tests():
--- katas/6 kyu/simple-encryption-number-1-alternating-split/description.md
+++ katas/6 kyu/simple-encryption-number-1-alternating-split/description.md
@@ -1,3 +1,4 @@
+# Simple Encryption #1 - Alternating Split
 Implement a pseudo-encryption algorithm which given a string `S` and an integer `N` concatenates all the odd-indexed characters of `S` with all the even-indexed characters of `S`, this process should be repeated `N` times.
 
 Examples:
