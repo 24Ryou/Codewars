70ffa01b2a4b60b90f41ac1a443dcd1681e6a9b1
24Ryou <a.yousefzadeh24@gmail.com>
2022-10-13 13:54:50 +0330

    PHP - Stop gninnipS My sdroW!

--- app/kata.js
+++ app/kata.js
@@ -1,38 +1,7 @@
-// rock-paper-scissors
+// string-repeat
 /* ------------------------------- MY SOLUTION ------------------------------ */
-const rules = {
-  "rock" : "paper",
-  "paper" : "scissors",
-  "scissors" : "rock"
-}
-const rps = (p1, p2) => {
-  if (rules[p1] === rules[p2]) return "Draw!" 
-  return `Player ${rules[p1] === p2 ? 2:1} won!`
-}
 /* ----------------------------- CLEVER SOLUTION ---------------------------- */
 /* ---------------------------------- TEST ---------------------------------- */
-const chai = require("chai");
-const Test = chai.assert.strictEqual; // replace Test.assertEqual with Test
-chai.config.truncateThreshold=0;
-
-describe('rock paper scissors', function() {
-  const getMsg = (n) => `Player ${n} won!`;
-
-  it('player 1 win', function() {
-    Test(rps('rock', 'scissors'), getMsg(1));
-    Test(rps('scissors', 'paper'), getMsg(1));
-    Test(rps('paper', 'rock'), getMsg(1));
-  });
-
-  it('player 2 win', function() {
-    Test(rps('scissors', 'rock'), getMsg(2));
-    Test(rps('paper', 'scissors'), getMsg(2));
-    Test(rps('rock', 'paper'), getMsg(2));
-  });
-
-  it('draw', function() {
-    Test(rps('rock', 'rock'), 'Draw!');
-    Test(rps('scissors', 'scissors'), 'Draw!');
-    Test(rps('paper', 'paper'), 'Draw!');
-  });
-});
+// const chai = require("chai");
+// const Test = chai.assert.strictEqual; // replace Test.assertEqual with Test
+// chai.config.truncateThreshold=0;
