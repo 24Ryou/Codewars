8edc646d383b0399fad5b513048bf17bd1d1ca03
24Ryou <a.yousefzadeh24@gmail.com>
2022-10-21 09:33:15 +0330

    PHP - Reversed Strings

--- app/kataTest.php
+++ app/kataTest.php
@@ -2,5 +2,16 @@
 <?php
 use PHPUnit\Framework\TestCase;
 /* ------------------------------- MY SOLUTION ------------------------------ */
+function solution($str) {
+  return strrev($str);
+}
 /* ----------------------------- CLEVER SOLUTION ---------------------------- */
 /* ---------------------------------- TEST ---------------------------------- */
+class ReversedStringsTest extends TestCase {
+  public function testExamples() {
+    $this->assertSame("dlrow", solution("world"));
+    $this->assertSame("olleh", solution("hello"));
+    $this->assertSame("", solution(""));
+    $this->assertSame('h', solution("h"));
+  }
+}
