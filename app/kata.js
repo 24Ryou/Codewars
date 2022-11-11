72787cf9bd4358810e189ba48db675af8769db3d
24ryou <a,yousefzadeh24@gmail.com>
2022-11-11 13:24:14 +0330

    JavaScript - Array.diff

--- app/kata.js
+++ app/kata.js
@@ -1,29 +1,25 @@
-// are-they-the-same
+// array-dot-diff
 /* ------------------------------- MY SOLUTION ------------------------------ */
-function comp(array1, array2){
-  return array1 && array2 !== null ? array1.map(x => x*x).sort().toString() === array2.sort().toString() : false
+function arrayDiff(a, b) {
+  return a.filter(x => !b.includes(x))
 }
 /* ----------------------------- CLEVER SOLUTION ---------------------------- */
-function comp(array1, array2) {
-  if(array1 == null || array2 == null) return false;
-  array1.sort((a, b) => a - b); array2.sort((a, b) => a - b);
-  return array1.map(v => v * v).every((v, i) => v == array2[i]);
-}
-
-function comp(a, b) {
-  return !!a && !!b && a.map(x => x*x).sort().join() == b.sort().join();
-}
 /* ---------------------------------- TEST ---------------------------------- */
 // const chai = require("chai");
 // const Test = chai.assert.strictEqual; // replace Test.assertEqual with Test
 // chai.config.truncateThreshold=0;
 
-const { assert } = require('chai');
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
 
-describe("Tests", () => {
-  it("test", () => {
-    let a1 = [121, 144, 19, 161, 19, 144, 19, 11];
-    let a2 = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19];
-    assert.isTrue(comp(a1, a2));
+describe("Basic tests", () => {
+  it("Should pass Basic tests", () => {
+    assert.deepEqual(arrayDiff([1,2], [1]), [2], "a was [1,2], b was [1]");
+    assert.deepEqual(arrayDiff([1,2,2], [1]), [2,2], "a was [1,2,2], b was [1]");
+    assert.deepEqual(arrayDiff([1,2,2], [2]), [1], "a was [1,2,2], b was [2]");
+    assert.deepEqual(arrayDiff([1,2,2], []), [1,2,2], "a was [1,2,2], b was []");
+    assert.deepEqual(arrayDiff([], [1,2]), [], "a was [], b was [1,2]");
+    assert.deepEqual(arrayDiff([1,2,3], [1,2]), [3], "a was [1,2,3], b was [1,2]")
   });
 });
